2acae5f42bc3748a03fb7eae1fb3f497d1c80d2e
M Murali Karthick <165348508+Murali47k@users.noreply.github.com>
2025-02-17 08:22:17 +0530

    ADDED STCF with IO

--- stcf.h
+++ stcf.h
@@ -13,5 +13,7 @@ extern int head, tail;
 // Function prototypes
 int find_shortest_job(int *duration, int head, int tail);
 void STCF(process **processes, int *duration, int size);
+int* calculate_first_job_duration(process **processes,int size);
+void STCF_IO(process **processes,int *duration,int size);
 
 #endif // STCF_H
