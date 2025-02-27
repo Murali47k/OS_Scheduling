b0f744f231493809e7212edadce24afe432d8fed
M Murali Karthick <165348508+Murali47k@users.noreply.github.com>
2025-02-27 22:35:07 +0530

    STCF_IO updated

--- stcf.h
+++ stcf.h
@@ -13,7 +13,6 @@ extern int head, tail;
 // Function prototypes
 int find_shortest_job(int *duration, int head, int tail);
 void STCF(process **processes, int *duration, int size);
-int* calculate_first_job_duration(process **processes,int size);
-void STCF_IO(process **processes,int *duration,int size);
+void STCF_IO(process **processes,int size);
 
 #endif // STCF_H
